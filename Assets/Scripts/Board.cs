3d0898a0c213d010e74fd0d213cc416419f0be71
fusil5577 <qwet676@gmail.com>
2024-04-16 15:16:42 +0900

    카드 맞추기
    이름과 실패 표시는 시간 만들어지고 만들겠습니다!

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -21,5 +21,7 @@ public class Board : MonoBehaviour
             go.transform.position = new Vector2(x, y);
             go.GetComponent<Card>().Setting(arr[i]);
         }
+
+        GameManager.instance.cardCount = arr.Length;
     }
 }
